838a6d8e6af1b65cc07458536c580b97bd9b510b
kysmkzs1 <masa@mealthy.me>
2015-08-26 18:42:47 +0000

    add profile area

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,11 @@ class UsersController < ApplicationController
   def show # 追加
    @user = User.find(params[:id])
   end
+
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -12,6 +14,21 @@ class UsersController < ApplicationController
       redirect_to @user # ここを修正
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      # 保存に成功した場合はユーザーページへリダイレクト
+      redirect_to edit_user_path , notice: 'プロフィールを更新しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
     end
   end
 
