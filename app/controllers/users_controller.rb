ba22795650d388153febacd3fab1b5c86dcd6f03
kysmkzs1 <masa@mealthy.me>
2015-08-29 14:30:14 +0000

    modify private

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
+    params.require(:user).permit(:name, :email, :profile, :area, :web, :bd, :password,
                                  :password_confirmation)
   end
 end
